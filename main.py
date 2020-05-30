e79e8b877592c3dbe1df07c636db25ea2904663c
AbhimanyuShahi <63775603+Abhimanyushahi@users.noreply.github.com>
2020-05-30 17:17:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from keras.preprocessing import image
 
 (x_train, y_train) , (x_test, y_test) = mnist.load_data()
 x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(100000,784)
+x_test = x_test.reshape(10000,784)
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
